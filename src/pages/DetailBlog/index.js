7090c7901555da600c79f39b733129519b1f648d
unknown <hello.safrian@gmail.com>
2021-09-06 14:32:43 +0700

    styling halaman detail blog post

--- src/pages/DetailBlog/index.js
+++ src/pages/DetailBlog/index.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { RegisterBg } from '../../assets';
+import './detailBlog.scss';
 
 const DetailBlog = () => {
     return (
-        <div>
-            <p>Content Detail Blog</p>
+        <div className="detail-blog-wrapper">
+            <img className="img-cover" src={RegisterBg} alt="thumb"/>
+            <h3 className="blog-title">Title Blog</h3>
+            <p className="blog-author">Author - Date Post</p>
+            <p className="blog-body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam architecto blanditiis quos veniam praesentium expedita alias necessitatibus inventore hic voluptatibus. Mollitia, incidunt sequi velit corporis reiciendis voluptatibus, consectetur expedita tempore dignissimos fuga laudantium! Ea officiis quas temporibus, ex quod saepe repellat atque tempora, est maxime velit quidem placeat ipsum eum possimus delectus. A exercitationem accusamus alias possimus cum voluptates excepturi? Temporibus doloremque assumenda nulla recusandae, maiores facere dolorem quaerat asperiores!</p>
         </div>
     )
 }
