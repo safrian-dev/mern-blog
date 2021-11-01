3cc07b52be4af78bf72f379a1cc8e23f3927441b
unknown <hello.safrian@gmail.com>
2021-11-01 14:20:37 +0700

    refactor new post & redux

--- src/config/redux/reducer/reducer.js
+++ src/config/redux/reducer/reducer.js
@@ -1,7 +1,8 @@
-import { combineReducers } from "redux"
-import globalReducer from "./globalReducer"
-import homeReducer from "./homeReducer"
+import { combineReducers } from "redux";
+import globalReducer from "./globalReducer";
+import homeReducer from "./homeReducer";
+import createBlogReducer from "./createBlogReducer";
 
-const reducer = combineReducers({globalReducer, homeReducer});
+const reducer = combineReducers({globalReducer, homeReducer, createBlogReducer});
 
 export default reducer;
--- src/pages/CreateBlog/index.js
+++ src/pages/CreateBlog/index.js
@@ -1,42 +1,22 @@
-import React, { useState } from 'react'
 import './createblog.scss'
 import { Button, Input, Upload, Textarea, Gap, Link } from '../../components'
 import { useHistory } from 'react-router-dom'
-import axios from 'axios'
+import { useDispatch, useSelector } from 'react-redux'
+import { postToApi, setForm, setImgPreview } from '../../config/redux/action'
 
 const CreateBlog = () => {
-    const [title, setTitle] = useState('');
-    const [body, setBody] = useState('');
-    const [image, setImage] = useState('');
-    const [imagePreview, setImagePreview] = useState(null);
+    const {form, imgPreview} = useSelector(state => state.createBlogReducer);
+    const {title, body} = form;
+    const dispatch = useDispatch();
 
     const onSubmit = () => {
-        console.log('title : ', title);
-        console.log('body : ', body);
-        console.log('image : ', image);
-
-        const data = new FormData();
-        data.append('title', title);
-        data.append('body', body);
-        data.append('image', image);
-
-        axios.post('http://localhost:4000/v1/blog/post', data, {
-            headers: {
-                'content-type': 'multipart/form-data'
-            }
-        })
-        .then(res => {
-            console.log('post success. ' , res);
-        })
-        .catch(err => {
-            console.log('err :' , err);
-        })
+        postToApi(form)
     }
 
     const onImageUpload = (e) => {
         const file = e.target.files[0];
-        setImage(file);
-        setImagePreview(URL.createObjectURL(file));
+        dispatch(setForm('image', file));
+        dispatch(setImgPreview(URL.createObjectURL(file)));
     }
 
     const history = useHistory();
@@ -45,11 +25,11 @@ const CreateBlog = () => {
             <Link title="Create Blog" onClick={() => history.push('/')} />
             <p className="title">Create New Blog Post</p>
 
-            <Input label="Post Title" value={title} onChange={(e) => setTitle(e.target.value)} />
+            <Input label="Post Title" value={title} onChange={(e) => dispatch(setForm('title', e.target.value))} />
 
-            <Upload onChange={(e) => onImageUpload(e)} img={imagePreview}/>
+            <Upload onChange={(e) => onImageUpload(e)} img={imgPreview}/>
             
-            <Textarea value={body} onChange={(e) => setBody(e.target.value)}/>
+            <Textarea value={body} onChange={(e) => dispatch(setForm('body', e.target.value))}/>
             
             <Gap height={20} />
             <div className="button-action">
