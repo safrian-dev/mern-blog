970a7052da4d4180da4d56f308303e0814ae32a3
unknown <hello.safrian@gmail.com>
2021-11-29 08:42:28 +0700

    update blog post

--- src/config/redux/action/createBlogAction.js
+++ src/config/redux/action/createBlogAction.js
@@ -1,33 +1,56 @@
 import axios from "axios";
 
 export const setForm = (formType, formValue) => {
-    return {
-        type: 'SET_FORM_DATA', formType, formValue
-    }
-}
+  return {
+    type: "SET_FORM_DATA",
+    formType,
+    formValue,
+  };
+};
 
 export const setImgPreview = (payload) => {
-    return {
-        type: 'SET_IMG_PREVIEW', payload
-    }
-}
+  return {
+    type: "SET_IMG_PREVIEW",
+    payload,
+  };
+};
 
 export const postToApi = (form) => {
+  const data = new FormData();
+  data.append("title", form.title);
+  data.append("body", form.body);
+  data.append("image", form.image);
 
-    const data = new FormData();
-    data.append('title', form.title);
-    data.append('body', form.body);
-    data.append('image', form.image);
+  axios
+    .post("http://localhost:4000/v1/blog/post", data, {
+      headers: {
+        "content-type": "multipart/form-data",
+      },
+    })
+    .then((res) => {
+      console.log("post success. ", res);
+    })
+    .catch((err) => {
+      console.log("err :", err);
+    });
+};
 
-    axios.post('http://localhost:4000/v1/blog/post', data, {
-        headers: {
-            'content-type': 'multipart/form-data'
-        }
+export const updateToApi = (form, id) => {
+  const data = new FormData();
+  data.append("title", form.title);
+  data.append("body", form.body);
+  data.append("image", form.image);
+
+  axios
+    .put(`http://localhost:4000/v1/blog/post/${id}`, data, {
+      headers: {
+        "content-type": "multipart/form-data",
+      },
     })
-    .then(res => {
-        console.log('post success. ' , res);
+    .then((res) => {
+      console.log("update success. ", res);
     })
-    .catch(err => {
-        console.log('err :' , err);
-    })
-}
+    .catch((err) => {
+      console.log("err :", err);
+    });
+};
--- src/pages/CreateBlog/index.js
+++ src/pages/CreateBlog/index.js
@@ -1,43 +1,84 @@
-import './createblog.scss'
-import { Button, Input, Upload, Textarea, Gap, Link } from '../../components'
-import { useHistory } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { postToApi, setForm, setImgPreview } from '../../config/redux/action'
+import "./createblog.scss";
+import React, { useEffect, useState } from "react";
+import { Button, Input, Upload, Textarea, Gap, Link } from "../../components";
+import { useHistory, withRouter } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  postToApi,
+  updateToApi,
+  setForm,
+  setImgPreview,
+} from "../../config/redux/action";
+import Axios from "axios";
 
-const CreateBlog = () => {
-    const {form, imgPreview} = useSelector(state => state.createBlogReducer);
-    const {title, body} = form;
-    const dispatch = useDispatch();
+const CreateBlog = (props) => {
+  const { form, imgPreview } = useSelector((state) => state.createBlogReducer);
+  const { title, body } = form;
+  const dispatch = useDispatch();
+  const [isUpdate, setIsUpdate] = useState(false);
+  const history = useHistory();
 
-    const onSubmit = () => {
-        postToApi(form)
+  useEffect(() => {
+    console.log("params :", props);
+    const id = props.match.params.id;
+    if (id) {
+      setIsUpdate(true);
+      Axios.get(`http://localhost:4000/v1/blog/post/${id}`)
+        .then((res) => {
+          const data = res.data.data;
+          console.log("data : ", data);
+          dispatch(setForm("title", data.title));
+          dispatch(setForm("body", data.body));
+          dispatch(setImgPreview(`http://localhost:4000/${data.image}`));
+        })
+        .catch((err) => {
+          console.log("err : ", err);
+        });
     }
+  }, [props, dispatch]);
 
-    const onImageUpload = (e) => {
-        const file = e.target.files[0];
-        dispatch(setForm('image', file));
-        dispatch(setImgPreview(URL.createObjectURL(file)));
+  const onSubmit = () => {
+    const id = props.match.params.id;
+    if (isUpdate) {
+      console.log("update data");
+      updateToApi(form, id);
+    } else {
+      console.log("create data");
+      postToApi(form);
     }
+  };
 
-    const history = useHistory();
-    return (
-        <div className="blog-post">
-            <Link title="Create Blog" onClick={() => history.push('/')} />
-            <p className="title">Create New Blog Post</p>
+  const onImageUpload = (e) => {
+    const file = e.target.files[0];
+    dispatch(setForm("image", file));
+    dispatch(setImgPreview(URL.createObjectURL(file)));
+  };
 
-            <Input label="Post Title" value={title} onChange={(e) => dispatch(setForm('title', e.target.value))} />
+  return (
+    <div className="blog-post">
+      <Link title="Create Blog" onClick={() => history.push("/")} />
+      <p className="title">{isUpdate ? "Update" : "Create New"} Blog Post</p>
 
-            <Upload onChange={(e) => onImageUpload(e)} img={imgPreview}/>
-            
-            <Textarea value={body} onChange={(e) => dispatch(setForm('body', e.target.value))}/>
-            
-            <Gap height={20} />
-            <div className="button-action">
-                <Button title="Save" onClick={onSubmit} />
-            </div>
-            <Gap height={20} />
-        </div>
-    )
-}
+      <Input
+        label="Post Title"
+        value={title}
+        onChange={(e) => dispatch(setForm("title", e.target.value))}
+      />
 
-export default CreateBlog
+      <Upload onChange={(e) => onImageUpload(e)} img={imgPreview} />
+
+      <Textarea
+        value={body}
+        onChange={(e) => dispatch(setForm("body", e.target.value))}
+      />
+
+      <Gap height={20} />
+      <div className="button-action">
+        <Button title={isUpdate ? "Update" : "Simpan"} onClick={onSubmit} />
+      </div>
+      <Gap height={20} />
+    </div>
+  );
+};
+
+export default withRouter(CreateBlog);
